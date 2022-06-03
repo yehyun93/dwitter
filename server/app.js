940a9a040c1a749fa3ac34aa8171816e7b541b46
YeHyun <104401577+yehyun93@users.noreply.github.com>
2022-06-04 00:52:20 +0900

    드위터 rest api 구현

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan('combined'));
 
-const tweets = [
+let tweets = [
   {
     "id" : "1",
     "text" : "공부하기시렁",
@@ -22,6 +22,14 @@ const tweets = [
     "name" : "node_study22",
     "username" : "user2",
     "url" : "test2.jpg"
+  },
+  {
+    "id" : "3",
+    "text" : "공부하기시렁22",
+    "createdAt" : "2022-06-04",
+    "name" : "하이잉",
+    "username" : "user1",
+    "url" : "test2.jpg"
   }
 ];
 
@@ -29,15 +37,63 @@ app.get('/tweets', (req, res) => {
   console.log(req.query.username);
 
   if(req.query.username) {
-    foreach(tweets => )
+    const username = req.query.username;
+    const userTweets = tweets.filter((tweets) => tweets.username === username);
+
+    res.send(userTweets);
+  } else {
+    res.send(tweets);
+  }
+});
+
+app.get('/tweets/:id', (req, res) => {
+  const tweetId = req.params.id;
+  const tweetById = tweets.filter((tweets) => tweets.id === tweetId);
+
+  res.send(tweetById);
+});
+
+app.post('/tweets', (req, res) => {
+  const newTweet = {
+    "id": "4",
+    "text": req.body.text,
+    "createdAt": getToday(),
+    "username": req.body.username,
+    "name": req.body.name,
+    "url": "test3.txt"
   }
 
-  res.send(tweets);
+  tweets.push(newTweet);
+  res.status(201).send(newTweet);
 });
 
-app.get('/tweets', (req, res) => {
-
+app.put('/tweets/:id', (req, res) => {
+  const tweetId = req.params.id;
+  
+  tweets.forEach(tweet => {
+    if(tweet.id === tweetId) {
+      tweet.text = req.body.text;
+      res.status(200).send(tweet);
+      return;
+    }
+  });
 });
 
+app.delete('/tweets/:id', (req, res) => {
+  const tweetId = req.params.id;
 
-app.listen(3000);
+  tweets = tweets.filter((tweets) => tweets.id != tweetId);
+  console.log(tweets);
+  res.status(200).send();
+});
+
+function getToday() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = ('0' + (today.getMonth() + 1)).slice(-2);
+  const day = ('0' + today.getDate()).slice(-2);
+
+  return year + '-' + month  + '-' + day;
+}
+
+app.listen(8080);
